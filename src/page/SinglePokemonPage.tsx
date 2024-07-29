32f0d26026dd4b9f18f16538124851185c5c1561
Christian Scholtysik <christianscholtysik@gmail.com>
2024-07-29 11:56:37 +0200

    done

--- src/page/SinglePokemonPage.tsx
+++ src/page/SinglePokemonPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { IPokemon } from "../interface/IPokemon";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const SinglePokemonPage = () => {
   const [singlePokemonDetail, setSinglePokemonDetail] =
@@ -19,32 +19,34 @@ const SinglePokemonPage = () => {
   console.log(singlePokemonDetail);
 
   return (
-    <>
-      <h1>{singlePokemonDetail?.name}</h1>
-      <Link to="/pokemon">Back</Link>
-      <img src={singlePokemonDetail?.sprites.front_default} alt="" />
-      <p>{singlePokemonDetail?.height}</p>
-
-      <div className="card bg-base-100 w-96 shadow-xl">
+    <section
+      className=" card-wrapper
+    ">
+      <div className="card bg-stone-100 w-96 shadow-xl">
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
+            src={singlePokemonDetail?.sprites.front_default}
+            alt={singlePokemonDetail?.name}
+            className="image"
           />
         </figure>
         <div className="card-body">
           <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
+            {singlePokemonDetail?.name.toUpperCase()}
+            <div className="badge badge-secondary">
+              {singlePokemonDetail?.base_experience}
+            </div>
           </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p> {singlePokemonDetail?.weight} g</p>{" "}
+          <p>{singlePokemonDetail?.height} cm </p>
+          <p>Base Experince: {singlePokemonDetail?.base_experience}</p>
           <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+            <div className="badge badge-outline badge-primary">hello</div>
+            <div className="badge badge-outline badge-secondary">Products</div>
           </div>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
