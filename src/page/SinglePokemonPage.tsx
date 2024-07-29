678f57a37a8e04183776223260058c2a594b43b8
Christian Scholtysik <christianscholtysik@gmail.com>
2024-07-29 11:30:59 +0200

    add daisy ui

--- src/page/SinglePokemonPage.tsx
+++ src/page/SinglePokemonPage.tsx
@@ -22,8 +22,28 @@ const SinglePokemonPage = () => {
     <>
       <h1>{singlePokemonDetail?.name}</h1>
       <Link to="/pokemon">Back</Link>
-      <p>{singlePokemonDetail?.height}</p>
       <img src={singlePokemonDetail?.sprites.front_default} alt="" />
+      <p>{singlePokemonDetail?.height}</p>
+
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Shoes!
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
